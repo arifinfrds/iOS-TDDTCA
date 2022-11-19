8e582875d0f37b803b17c3be0db7f8de040464ed
arifinfrds <arifinfrds.engineer@gmail.com>
2022-11-19 16:31:28 +0700

    cover the scenario when the button login tapped, should show error message

--- TDDTCATests/LoginScreenReducerTests.swift
+++ TDDTCATests/LoginScreenReducerTests.swift
@@ -12,11 +12,13 @@ struct LoginScreenFeature: ReducerProtocol {
     struct State: Equatable {
         var emailText: String = ""
         var passwordText: String = ""
+        var errorMessage: String?
     }
     
     enum Action {
         case onEmailTextFieldChanged(text: String)
         case onPasswordTextFieldChanged(text: String)
+        case onLoginButtonTapped
     }
     
     func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
@@ -28,6 +30,10 @@ struct LoginScreenFeature: ReducerProtocol {
             
         case let .onPasswordTextFieldChanged(text):
             state.passwordText = text
+            return .none
+            
+        case .onLoginButtonTapped:
+            state.errorMessage = "Email and password should not be empty"
             return .none
         }
     }
@@ -56,6 +62,17 @@ final class LoginScreenReducerTests: XCTestCase {
         
         store.send(.onPasswordTextFieldChanged(text: typedText)) { state in
             state.passwordText = typedText
+        }
+    }
+    
+    func test_loginButtonTapped_showsErrorOnEmptyFields() {
+        let store = TestStore(
+            initialState: LoginScreenFeature.State(),
+            reducer: LoginScreenFeature()
+        )
+        
+        store.send(.onLoginButtonTapped) { state in
+            state.errorMessage = "Email and password should not be empty"
         }
     }
 }
