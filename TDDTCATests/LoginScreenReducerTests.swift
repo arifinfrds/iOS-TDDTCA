065275ac05a1dc79f2fcb157077a0d602cdfb0d8
arifinfrds <arifinfrds.engineer@gmail.com>
2022-11-19 16:23:28 +0700

    onPasswordTextField change should save password text state

--- TDDTCATests/LoginScreenReducerTests.swift
+++ TDDTCATests/LoginScreenReducerTests.swift
@@ -11,10 +11,12 @@ import XCTest
 struct LoginScreenFeature: ReducerProtocol {
     struct State: Equatable {
         var emailText: String = ""
+        var passwordText: String = ""
     }
     
     enum Action {
         case onEmailTextFieldChanged(text: String)
+        case onPasswordTextFieldChanged(text: String)
     }
     
     func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
@@ -22,6 +24,10 @@ struct LoginScreenFeature: ReducerProtocol {
             
         case let .onEmailTextFieldChanged(text):
             state.emailText = text
+            return .none
+            
+        case let .onPasswordTextFieldChanged(text):
+            state.passwordText = text
             return .none
         }
     }
@@ -38,6 +44,18 @@ final class LoginScreenReducerTests: XCTestCase {
         
         store.send(.onEmailTextFieldChanged(text: typedText)) { state in
             state.emailText = typedText
+        }
+    }
+    
+    func test_onPasswordTextFieldChanged_shouldSaveEmailState() {
+        let typedText = "password"
+        let store = TestStore(
+            initialState: LoginScreenFeature.State(),
+            reducer: LoginScreenFeature()
+        )
+        
+        store.send(.onPasswordTextFieldChanged(text: typedText)) { state in
+            state.passwordText = typedText
         }
     }
 }
